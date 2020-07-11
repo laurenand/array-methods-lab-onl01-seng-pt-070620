b92bc686e5a2c4340d823a3eef12408e4b77c88c
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-11 01:14:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,25 @@
 def using_include(array, element)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include? ("wow")
 end
 
 def using_sort(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.sort
 end
 
 def using_reverse(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_first(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_last(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_size(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
